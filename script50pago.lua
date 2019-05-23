70b3f9214565a952195855778c5dcb5ecfa545d0
trevashacks <50086597+trevashacks@users.noreply.github.com>
2019-05-22 23:46:00 -0300

    Update script50pago.lua

--- script50pago.lua
+++ script50pago.lua
@@ -1,2 +1,341 @@
-gg.alert("M A N U N T E N Ç Ã O")
-gg.alert("FORAM ENCONTRADOS OUTROS DIRETÓRIOS com.netease.ko EM MODIFICAÇÃO DA PASTA DE ACORDO COM O IP VALIDADO NA COMPRA.")
+gg.alert("ENQUANTO TO ATUALIZANDO A NOVA ATT VOU DEIXAR ESSA ANTIGA POR ENQUANTO, CUIDADO Q ALGUNS HACKS TAO DANDO BAN")
+goto START
+::START::
+menu= gg.choice({
+'➀.♦ʀᴀᴅᴀʀ ʜᴀᴄᴋ(ᴀᴛɪᴠᴀʀ ɴᴏ ʟᴏʙʙʏ)♦'
+,'➁.♠sᴜᴘᴇʀ ᴘᴜʟᴏ(ᴀʟᴛᴇʀᴀʀ ᴍᴀɴᴜᴀʟᴍᴇɴᴛᴇ ᴏ ᴠᴀʟᴏʀ 3 ᴘᴀʀᴀ 6)♠'
+,'➂.♦ʀᴇᴄᴀʀʀᴇɢᴀʀ ᴇᴍ 1sᴇɢᴜɴᴅᴏ♦'
+,'➃.♠ᴍ4ᴀ1 ᴀɪᴍʙᴏᴛ(ᴀᴛɪᴠᴇ ᴄᴏᴍ ᴀ ᴍ4ᴀ1 ᴇᴍ ᴍᴀᴏs)♠'
+,'➄.♦ᴘᴜʟᴀʀ ɪɴғɪɴɪᴛᴀs ᴠᴇᴢᴇs ɴᴏ ᴀʀ♦'
+,'➅.♠ʙᴀʟᴀ ᴀᴛʀᴀᴠᴇssᴀ ᴘᴀʀᴇᴅᴇ(ɴᴏᴠᴏ)♠'
+,'➆.♦ᴍ4 ɴᴏ ʀᴇᴄᴏɪʟ(ᴀᴛɪᴠᴀʀ ɴᴀ ᴛᴇʟᴀ ᴅᴇ ʟᴏɢɪɴ ᴇ ᴀʟᴛᴇʀᴀʀ ᴛᴏᴅᴏs ᴏs ᴠᴀʟᴏʀᴇs ᴘᴀʀᴀ 0 ᴍᴀɴᴜᴀʟᴍᴇɴᴛᴇ)♦'
+,'➇.♠ᴀᴋ ɴᴏ ʀᴇᴄᴏɪʟ(ᴀᴛɪᴠᴀʀ ɴᴀ ᴛᴇʟᴀ ᴅᴇ ʟᴏɢɪɴ ᴇ ᴀʟᴛᴇʀᴀʀ ᴛᴏᴅᴏs ᴏs ᴠᴀʟᴏʀᴇs ᴘᴀʀᴀ 0 ᴍᴀɴᴜᴀʟᴍᴇɴᴛᴇ)♠'
+,'➈.♦ɢᴀsᴏʟɪɴᴀ ɪɴғɪɴɪᴛᴀ♦'
+,'➀⓪.♠ɢɪɢᴀɴᴛᴇ♠'
+,'➀➀.♦ᴇsᴄᴀʟᴀʀ ᴘᴀʀᴇᴅᴇ♦'
+,'➀➁.♠ᴡᴀʟʟʜᴀᴄᴋ(sɴᴀᴘᴅʀᴀɢᴏɴ625-636)♠'
+,'➀➂.♦ʀᴇᴄᴜᴘᴇʀᴀçᴀᴏ ɪᴍᴇᴅɪᴀᴛᴀ(ᴀᴛɪᴠᴀʀ ɴᴀ ᴛᴇʟᴀ ᴅᴇ ʟᴏɢɪɴ, sᴇ ᴀᴘᴀʀᴇᴄᴇʀ sᴏᴍᴇɴᴛᴇ 3 ᴠᴀʟᴏʀᴇs, ᴀʟᴛᴇʀᴇ ᴛᴏᴅᴏs ᴘᴀʀᴀ 0)♦'
+,'➀➃.♠ᴇɴᴛʀᴀʀ ᴇᴍʙᴀɪxᴏ ᴅᴀ ᴛᴇʀʀᴀ(ᴜɴᴅᴇʀɢʀᴏᴜɴᴅ)♠'
+,'➀➄.♦ᴀᴛɪᴠᴀʀ ᴀɴᴛᴇs ᴅᴏ sᴘᴇᴇᴅ, ᴇssᴇ sᴇʀᴠᴇ ᴘᴀʀᴀ ᴏ sᴘᴇᴇᴅ ɴᴀᴏ ғɪᴄᴀʀ ᴠᴏʟᴛᴀɴᴅᴏ ᴘᴀʀᴀ ᴛʀᴀs♦'
+,'➀➅.♦ʀᴇᴍᴏᴠᴇʀ ɢʀᴀᴍᴀ♦'
+,'➀➆.♠ᴀᴄᴇʟᴇʀᴀçᴀᴏ ᴅᴇ ɴᴀᴛᴀçãᴏ♠'
+,'➀➇.♦ᴀᴄᴇʟᴇʀᴀçᴀᴏ ᴅᴏ ᴘᴇʀsᴏɴᴀɢᴇᴍ ᴘᴀʀᴀ ᴛʀᴀs♦'
+,'➀➈.♠sᴘᴇᴇᴅ ʜᴀᴄᴋ ᴄᴏᴍ ᴀʀᴍᴀ ɴᴀ ᴍᴀᴏ♠'
+,'➁⓪.♦ʟᴏᴏᴛᴇᴀʀ ᴀ ᴜᴍᴀ ᴅɪsᴛᴀɴᴄɪᴀ ᴍᴀɪᴏʀ♦'
+,'➁➀.♠ᴘᴇʀsᴏɴᴀɢᴇᴍ ᴀᴛʀᴀᴠᴇssᴀ ᴘᴀʀᴇᴅᴇ(1x ᴘᴏʀ ᴘᴀʀᴛɪᴅᴀ)♠'
+,'➁➁.♦ɴᴏᴠᴏ ᴡᴀʟʟʜᴀᴄᴋ♦'
+,'➁➂.♠ᴀɪᴍʙᴏᴛ ᴛᴏᴅᴀs ᴀs ᴀʀᴍᴀs(ᴀɪɴᴅᴀ ᴇᴍ ᴛᴇsᴛᴇ, ᴀʟᴛᴇʀᴀʀ ᴛᴏᴅᴏs ᴠᴀʟᴏʀᴇs ᴅᴏᴜʙʟᴇ ᴘᴀʀᴀ 999999)♠'
+,'➁➃.♦ᴘᴇʀsᴏɴᴀɢᴇᴍ ᴘᴜʟᴀ ʟᴏɴɢᴇ♦'
+,'➁➄.♠ᴇɴᴛʀᴀʀ ᴇᴍʙᴀɪxᴏ ᴅᴀ ᴛᴇʀʀᴀ 1(ᴀᴛɪᴠᴀʀ ᴀɴᴛᴇs ᴅᴀ ᴛᴇʟᴀ ᴅᴇ ʟᴏɢɪɴ ᴇ ᴀᴛɪᴠᴀʀ ᴏ ᴠᴀʟᴏʀ 9,8 ᴘᴀʀᴀ 0)♠'
+,'➁➅.♦ᴇɴᴛʀᴀʀ ᴇᴍʙᴀɪxᴏ ᴅᴀ ᴛᴇʀʀᴀ 2(ᴀᴛɪᴠᴀʀ ɴᴀ ᴛᴇʟᴀ ᴅᴇ ʟᴏɢɪɴ)♦'
+,'➁➆.♠ᴇɴᴛʀᴀʀ ᴇᴍʙᴀɪxᴏ ᴅᴀ ᴛᴇʀʀᴀ 3(ᴀᴛɪᴠᴀʀ ɴᴀ ᴘᴀʀᴛɪᴅᴀ)♠'
+,'➁➇.♦ʙᴀʟᴀ ᴀᴛʀᴀᴠᴇssᴀ ᴘᴀʀᴇᴅᴇ(somente a parede)♦'
+,'➁➈.♠ᴍᴀᴘᴀ sᴇᴍ ɴᴇᴠᴏᴀ♠'
+,'➂⓪.♦ᴍᴀᴘᴀ ᴄᴏᴍ ᴄᴇᴜ ᴇsᴄᴜʀᴏ♦'
+,'➂➀.♠ᴄᴏʀ ɴᴏ ᴘᴇʀsᴏɴᴀɢᴇᴍ(ᴄʜᴀᴍs)-> Talvez seja necessário um CPU adequado♠'
+,'Exit'},nil,'♠ⓣⓡⓔⓥⓐⓢ ⓗⓐⓒⓚⓢ ➂/➄♠')
+
+if menu == 1 then goto SS1 end
+if menu == 2 then goto SS2 end
+if menu == 3 then goto SS3 end
+if menu == 4 then goto SS4 end
+if menu == 5 then goto SS5 end
+if menu == 6 then goto SS6 end
+if menu == 7 then goto SS7 end
+if menu == 8 then goto SS8 end
+if menu == 9 then goto SS9 end
+if menu == 10 then goto SS10 end
+if menu == 11 then goto SS11 end
+if menu == 12 then goto SS12 end
+if menu == 13 then goto SS13 end
+if menu == 14 then goto SS14 end
+if menu == 15 then goto SS15 end
+if menu == 16 then goto SS16 end
+if menu == 17 then goto SS17 end
+if menu == 18 then goto SS18 end
+if menu == 19 then goto SS19 end
+if menu == 20 then goto SS20 end
+if menu == 21 then goto SS21 end
+if menu == 22 then goto SS22 end
+if menu == 23 then goto SS23 end
+if menu == 24 then goto SS24 end
+if menu == 25 then goto SS25 end
+if menu == 26 then goto SS26 end
+if menu == 27 then goto SS27 end
+if menu == 28 then goto SS28 end
+if menu == 29 then goto SS29 end
+if menu == 30 then goto SS30 end
+if menu == 31 then goto SS31 end
+goto sdone
+
+::SS1::
+gg.clearResults()
+   gg.setRanges(gg.REGION_C_ALLOC)
+  gg.searchNumber("0.6;4.9e-324E;20;0.2;0.1::200", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+    gg.searchNumber("20", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+    gg.getResults(1)
+    gg.editAll("800", gg.TYPE_DOUBLE)
+    print("addListItems: ", gg.addListItems(MX))
+goto sdone
+::SS2::
+  gg.clearResults()
+  gg.searchNumber("40;2D;3;0.5;0.2::", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+ gg.getResults(10)
+gg.toast("Super Pulo")
+gg.toast("Alterar manualmente o 3 para 6")
+goto sdone
+::SS3::
+  gg.clearResults()
+  gg.setRanges(gg.REGION_C_ALLOC)
+  gg.searchNumber("-0.2015;1::", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("1", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(1)
+  gg.editAll("4", gg.TYPE_DOUBLE)
+  gg.toast("Recarregar rápido")
+goto sdone
+::SS4::
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("16D;1717986918D;1.67499995232", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("1.67499995232", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(10)
+gg.editAll("100000000", gg.TYPE_FLOAT)
+gg.toast("M4AIMBOT")
+goto sdone
+::SS5::
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("50.0F;-1D;0.5F;-1D;0.20000000298F:17", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("0.5", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(10)
+gg.editAll("999", gg.TYPE_FLOAT)
+goto sdone
+::SS6::
+gg.clearResults()
+ gg.setRanges(gg.REGION_CODE_APP | gg.REGION_CODE_SYS)
+gg.searchNumber("9.9999997e-10;9.9999997e-10;9.9999997e-10;9.9999997e-10:13", gg.TYPE_FLOAT)
+gg.getResults(1)
+gg.editAll(6666666666666666, gg.TYPE_FLOAT)
+gg.toast('Bala atravessa parede ativado com sucesso')
+gg.clearResults()
+gg.setRanges(gg.REGION_CODE_APP)
+gg.searchNumber("0.0000001", gg.TYPE_FLOAT)
+gg.getResults(2)
+gg.editAll(999, gg.TYPE_FLOAT)
+gg.clearResults()
+gg.toast('CanalYT: Trevas Hacks')
+goto sdone
+::SS7::
+  gg.clearResults()
+  gg.setRanges(gg.REGION_C_ALLOC)
+	 gg.searchNumber("3.5E;2.7E;1.8E;0.0154E::512", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.toast("M4 no recoil")
+goto sdone
+::SS8::
+gg.setRanges(gg.REGION_C_ALLOC)
+  gg.searchNumber("0.016E;-0.0695E;-0.0796E;2.3E;2.2E;1.7E::512", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+	gg.toast("ak-47 no recoil")
+goto sdone
+::SS9::
+gg.clearResults()
+  gg.setRanges(gg.REGION_C_DATA)
+  gg.searchNumber("-1;1,065,353,216D;1;5;4::100", gg.POINTER_WRITABLE, false, gg.SIGN_FUZZY_EQUAL, 0, -1)
+  gg.searchNumber("1", gg.POINTER_WRITABLE, false, gg.SIGN_FUZZY_EQUAL, 0, -1)
+  gg.getResults(2)
+  gg.editAll("0.001", gg.POINTER_WRITABLE)
+  gg.clearResults()
+  gg.toast("CARRO GASOLINA INFINITA")
+goto sdone
+::SS10::
+gg.clearResults()
+gg.setRanges(gg.REGION_C_BSS)
+gg.searchNumber("10D;1;1",gg.POINTER_WRITABLE,false,gg.SIGN_FUZZY_EQUAL,0,-1)
+gg.searchNumber("1",gg.POINTER_WRITABLE,false,gg.SIGN_FUZZY_EQUAL,0,-1)
+var = gg.getResults(50)
+gg.editAll("4",gg.POINTER_WRITABLE)
+  gg.toast("Gigante")
+goto sdone
+::SS11::
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("4.9e-324E;0.0078125E;1;0.20000000298;1.5F;65536D::500", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("0.2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(2)
+gg.toast("Escalar parede")
+goto sdone
+::SS12::
+  gg.clearResults()
+ gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("2.75000238419;2;-1.0;0.75;0.24899999797;0.99900001287::", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100)
+gg.editAll("90", gg.TYPE_FLOAT)
+gg.toast("Wall hack SNAPDRAGON 625-636")
+goto sdone
+::SS13::
+gg.clearResults()
+  gg.setRanges(gg.REGION_C_ALLOC)
+  gg.searchNumber("8.0E;1,075,838,976D;20.0E;6.0E;1,075,314,688D;10.0E;2.0E;0.0E;3.0E;1,074,266,112D;5.0", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("3;6;8", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(3)
+goto sdone
+::SS14::
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("6D;0.00999999978;1.5625;1::", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("0.00999999978", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(100)
+gg.editAll("0.00949999978", gg.TYPE_FLOAT)
+gg.toast("Entrar embaixo do subsolo")
+goto sdone
+::SS15::
+  gg.clearResults()
+  gg.setRanges(gg.REGION_C_ALLOC)
+  gg.searchNumber("0.25;0.625;0.1;0.5;1;2::", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("0.1", gg.TYPE_DOUBLE, false, gg.SIGN_DOUBLE, 0, -1)
+    gg.getResults(2)
+    gg.editAll("9999", gg.TYPE_DOUBLE)
+goto sdone
+::SS16::
+gg.clearResults()
+  gg.setRanges(gg.REGION_C_ALLOC)
+  gg.searchNumber("10D;5,126D;3D;11D;32D;4D::", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("10", gg.TYPE_DWORD, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(20)
+  gg.editAll("0", gg.TYPE_DWORD)
+gg.clearResults()
+goto sdone
+::SS17::
+  gg.clearResults()
+  gg.setRanges(gg.REGION_C_ALLOC)
+  gg.searchNumber("200;1.6;4.9e-324;2.09375F;2.5;2.0625F;4.4::", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+    gg.searchNumber("2.5", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+    gg.getResults(2)
+    gg.editAll("10", gg.TYPE_DOUBLE)
+    gg.toast("Acelerar natação")
+goto sdone
+::SS18::
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("3.5;2.1875F;5;3D:21", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("3.5;5", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(2)
+gg.editAll("10", gg.TYPE_DOUBLE)
+goto sdone
+::SS19::
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("4.9e-324E;1E;1.875;0.0001E;4.9e-324E;1.5E;1,073,217,536D:41", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("1.875", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(10)
+gg.editAll("1.9", gg.TYPE_FLOAT)
+goto sdone
+::SS20::
+gg.clearResults()
+  gg.setRanges(gg.REGION_C_ALLOC)
+  gg.searchNumber("5D;0E;0E;1F;2F::45", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(2)
+  gg.editAll("5.6", gg.TYPE_FLOAT)
+  gg.clearResults()
+  gg.toast("Looter a uma distancia maior")
+goto sdone
+   
+::SS21::
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("-1D; 10.0F; 0.40000000596F; 1.0F; 0.25F; 1D; 2D; 3D; 4D; 5D; 6D; 7D; 8D; 9D; 10D; 11D; 12D; 13D; 14D; ::500",gg.POINTER_WRITABLE,false,gg.SIGN_FUZZY_EQUAL,0,-1)
+gg.searchNumber("0.40000000596",gg.POINTER_WRITABLE,false,gg.SIGN_FUZZY_EQUAL,0,-1)
+gg.getResults(15)
+gg.editAll('-0,02679999918')
+goto sdone
+::SS22::
+gg.clearResults()
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("4,923D;853D;2F::150", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("2", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(20)
+gg.editAll("40", gg.TYPE_FLOAT)
+gg.clearResults()
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber('103,240D;2F::150',gg.TYPE_FLOAT,false,gg.SIGN_EQUAL,0,-1)
+gg.searchNumber('2',gg.TYPE_FLOAT, false, gg.SIGN_EQUAL,0,-1)
+gg.getResults(20)
+gg.editAll('15', gg.TYPE_FLOAT)
+goto sdone
+::SS23::
+gg.setRanges(gg.REGION_C_ALLOC)
+  gg.searchNumber("65,543D;5D;0.0078125E;0.01999999955F;2.0F::512", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+goto sdone
+::SS24::
+ gg.clearResults()
+  gg.setRanges(gg.REGION_C_ALLOC)
+  gg.searchNumber("2.1875F;1.75F;1.625F;2F;0.8E::110", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("0.8", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(1)
+gg.editAll(2)
+goto sdone
+::SS25::
+gg.clearResults()
+  gg.setRanges(gg.REGION_C_ALLOC)
+  gg.searchNumber("0.44720998406F;2.0E;2.0F;0.5E;9.8E;1.75F;9D:60", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+  gg.searchNumber("9.8", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+  gg.getResults(5)
+  gg.toast("xd")
+goto sdone
+::SS26::
+gg.clearResults()
+gg.setRanges(gg.REGION_C_BSS)
+gg.searchNumber("1,176,256,512D;0.0001F;1,176,256,512D;10,000.0F;1,176,256,512D;0.5F;0.0001F;1,1 76,256,512D::299", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("10000", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(15)
+gg.editAll("0", gg.TYPE_FLOAT)
+goto sdone
+::SS27::
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("\194\1600.70710682869;0.0078125E;1;0.00999999978;0.70710682869;0.0078125E;1,065,353,216D::330", gg.TYPE_FLOAT)
+gg.searchNumber("1", gg.TYPE_FLOAT)
+gg.getResults(10)
+goto sdone
+	
+::SS28::
+gg.setRanges(gg.REGION_CODE_APP)
+gg.searchNumber("9.9999997e-10;9.9999997e-10;9.9999997e-10;9.9999997e-10:13", gg.TYPE_FLOAT)
+gg.getResults(1)
+gg.editAll(6666666666666666, gg.TYPE_FLOAT)
+gg.toast('Bala atravessa parede ativado com sucesso')
+gg.clearResults()
+goto sdone
+
+::SS29::
+gg.setRanges(gg.REGION_CODE_APP)
+gg.searchNumber("0.002", gg.TYPE_FLOAT)
+gg.getResults(30)
+gg.editAll("0", gg.TYPE_FLOAT)
+gg.toast('Sem névoa')
+gg.clearResults()
+goto sdone
+
+::SS30::
+gg.setRanges(gg.REGION_CODE_APP)
+gg.searchNumber("0.09", gg.TYPE_FLOAT)
+gg.getResults(30)
+gg.editAll("-99", gg.TYPE_FLOAT)
+gg.toast('Céu negro')
+gg.clearResults()
+goto sdone
+
+::SS31::
+gg.setRanges(gg.REGION_BAD)
+gg.searchNumber("3.62945556641", gg.TYPE_FLOAT)
+gg.getResults(30)
+gg.editAll("991232519", gg.TYPE_FLOAT)
+gg.toast("chams")
+gg.clearResults()
+goto sdone
+
+::sdone::
+print('🇧🇷ⓒⓐⓝⓐⓛ ⓝⓞ ⓨⓞⓤⓣⓤⓑⓔ: ⓣⓡⓔⓥⓐⓢ ⓖⓞⓓ🇧🇷')
+os.exit()
