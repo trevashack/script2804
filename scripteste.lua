aeea506012b6afc340a2757294692b91a9507db2
trevashacks <50086597+trevashacks@users.noreply.github.com>
2019-07-02 15:34:21 -0300

    Update scripteste.lua

--- scripteste.lua
+++ scripteste.lua
@@ -1,13 +1,16 @@
 gg.alert('CHEAT FOR TEST | HACK PARA TESTE | 2019')
+gg.alert('Adicionei também bala atravessar parede p/ vcs brincar')
 goto START
 ::START::
 menu= gg.choice({
 '➀.♠WallHack Simples; FUNCIONANDO EM QUALQUER CELULAR♠'
-,'➁.♠ʙᴀʟᴀ ᴀᴛʀᴀᴠᴇssᴀ ᴘᴀʀᴇᴅᴇ(ɴᴏᴠᴏ)♠'
+,'➁.♠ʙᴀʟᴀ ᴀᴛʀᴀᴠᴇssᴀ ᴘᴀʀᴇᴅᴇ♠'
+,'➂.♠Todas as armas sem recoil♠'
 ,'Exit'},nil,'♠ⓣⓡⓔⓥⓐⓢ ⓗⓐⓒⓚⓢ ➁/➆♠')
 
 if menu == 1 then goto SS1 end
 if menu == 2 then goto SS2 end
+if menu == 3 then goto SS3 end
 goto sdone
 
 ::SS1::
@@ -33,6 +36,15 @@ gg.getResults(2)
 gg.editAll(999, gg.TYPE_FLOAT)
 gg.clearResults()
 gg.toast('CanalYT: Trevas Hacks')
+goto sdone
+
+::SS3::
+gg.clearResults()
+gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("1;700;1.8", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("700", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
+gg.getResults(5)
+gg.editAll("0", gg.TYPE_DOUBLE)
 goto sdone
 
 ::sdone::
