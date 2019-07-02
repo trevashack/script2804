3049318d6cf7a0bf0b4bc3e2329899fe63b18e1a
trevashacks <50086597+trevashacks@users.noreply.github.com>
2019-07-02 15:57:16 -0300

    Update scripteste.lua

--- scripteste.lua
+++ scripteste.lua
@@ -6,11 +6,16 @@ menu= gg.choice({
 '➀.♠WallHack Simples; FUNCIONANDO EM QUALQUER CELULAR♠'
 ,'➁.♠ʙᴀʟᴀ ᴀᴛʀᴀᴠᴇssᴀ ᴘᴀʀᴇᴅᴇ♠'
 ,'➂.♠Todas as armas sem recoil♠'
+,'➃.♠SupremeHack - Ative por partida♠(ATIVE NA SALA DE TREINO E ATIRE EM UM BOT PARA VER OQUE ACONTECE)'
+,'➄.♠WallHack PRO♠ - Não funciona em todos os celulares, esse wall fica invisível somente as pessoas. UTILIZE A VERSÃO DO GG 82.1+'
 ,'Exit'},nil,'♠ⓣⓡⓔⓥⓐⓢ ⓗⓐⓒⓚⓢ ➁/➆♠')
 
 if menu == 1 then goto SS1 end
 if menu == 2 then goto SS2 end
 if menu == 3 then goto SS3 end
+if menu == 4 then goto SS4 end
+if menu == 5 then goto SS5 end
+if menu == 6 then goto SS6 end
 goto sdone
 
 ::SS1::
@@ -45,6 +50,36 @@ gg.searchNumber("1;700;1.8", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
 gg.refineNumber("700", gg.TYPE_DOUBLE, false, gg.SIGN_EQUAL, 0, -1)
 gg.getResults(5)
 gg.editAll("0", gg.TYPE_DOUBLE)
+goto sdone
+
+::SS4::
+gg.clearResults()
+  gg.setRanges(gg.REGION_C_ALLOC)
+gg.searchNumber("0D;0.15000000596F;8,962D:29", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.refineNumber("0.15", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+revert = gg.getResults(9999, nil, nil, nil, nil, nil, nil, nil, nil)
+local t = gg.getResults(9999, nil, nil, nil, nil, nil, nil, nil, nil)
+for i, v in ipairs(t) do
+	if v.flags == gg.TYPE_FLOAT then
+		v.value = "4"
+		v.freeze = true
+	end
+end
+gg.addListItems(t)
+t = nil
+goto sdone
+
+::SS5::
+g.setRanges(gg.REGION_VIDEO)
+gg.clearResults()
+  gg.searchNumber("4923D;853D;2::150", gg.TYPE_FLOAT, false, gg.SIGN_EQUAL, 0, -1)
+gg.searchNumber("2", gg.TYPE_FLOAT, false, gg.SIGN_FLOAT, 0, -1)
+gg.getResults(500)
+gg.editAll("130", gg.TYPE_FLOAT)
+goto sdone
+
+::SS6::
+
 goto sdone
 
 ::sdone::
